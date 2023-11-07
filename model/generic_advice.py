5737fafbe9b6707f9f6ca2a5a180d362b306248c
paavansundar <paavan.duvvuri@gmail.com>
2023-11-07 19:05:04 +0530

    Update generic_advice.py

--- model/generic_advice.py
+++ model/generic_advice.py
@@ -22,3 +22,14 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 tokenizer = AutoTokenizer.from_pretrained("jirin/Llama-2-13b-fingpt2")
 model = AutoModelForCausalLM.from_pretrained("jirin/Llama-2-13b-fingpt2")
 
+from transformers import AutoTokenizer, LlamaForCausalLM
+
+model = LlamaForCausalLM.from_pretrained(PATH_TO_CONVERTED_WEIGHTS)
+tokenizer = AutoTokenizer.from_pretrained(PATH_TO_CONVERTED_TOKENIZER)
+
+prompt = "Hey, are you conscious? Can you talk to me?"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate
+generate_ids = model.generate(inputs.input_ids, max_length=30)
+tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
