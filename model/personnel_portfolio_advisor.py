c5620f5eb0b0389b5077ae77af3cc17b0e44e72b
paavansundar <paavan.duvvuri@gmail.com>
2023-11-20 14:41:17 +0530

    Update personnel_portfolio_advisor.py

--- model/personnel_portfolio_advisor.py
+++ model/personnel_portfolio_advisor.py
@@ -1,1 +1,13 @@
+from datetime import date
+import json
+from prophet_stock_trainer import StockTrainer
+import os
 
+class PersonnelAdvisor():
+  def getPortfolio(self):
+        trainer = StockTrainer(api_key, "tcs")
+        model, stock_history, stock_forecast = trainer.create_prophet_model(30)
+        train_mean_error, test_mean_error = trainer.evaluate_prediction()
+        title = 'Stock Prediction using Prophet for {} with mean error {:.2f}'.format(
+            symbol, test_mean_error)
+        print(title,stock_forecast[['ds', 'yhat']].to_json())
